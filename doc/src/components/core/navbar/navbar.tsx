400a4bd12cd81b4a5a4f0f53eedb6f196c309d7a
nicolacentonze <nicola.centonze@icloud.com>
2026-08-10 19:17:45 +0200

    navbar improvements

--- doc/src/components/core/navbar/navbar.tsx
+++ doc/src/components/core/navbar/navbar.tsx
@@ -2,10 +2,17 @@ import Link from "next/link";
 import classes from "./navbar.module.css";
 import Image from "next/image";
 import xeerpeLogo from "@assets/xeerpeIconLogo.png"
+import {Builder} from "xeerpe";
 
 const Navbar = () => {
+
+    const navbarBackground = new Builder()
+        .linearGradient({from: 'transparent', to: '#0a0f0a', angle: '135deg'})
+        .blur({type: 'backdrop', amount: '60px'})
+        .toStyle()
+
     return (
-        <header className={classes.header}>
+        <header className={classes.header} style={navbarBackground}>
             <Link href="/" className={classes.headerLogo}>
                 <Image className={classes.headerLogo} src={xeerpeLogo} alt="xeerpe logo"/>
             </Link>
